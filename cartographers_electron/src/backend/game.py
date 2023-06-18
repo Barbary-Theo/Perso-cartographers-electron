02dd8d7cf423148b5bedb72e460552d02e77209a
tba5767 <theo.barbary25@gmail.com>
2023-06-18 15:08:37 +0200

    implement main function on

--- cartographers_electron/src/backend/game.py
+++ cartographers_electron/src/backend/game.py
@@ -3,13 +3,22 @@ from model.cell_type import CellType
 from model.cell import Cell
 
 
+test_cell = [
+    [Cell(0, 0, CellType.FOREST), Cell(1, 0, CellType.EMPTY), Cell(2, 0, CellType.MOUNTAIN)],
+    [Cell(0, 1, CellType.MONSTER), Cell(1, 1, CellType.HOUSE), Cell(2, 1, CellType.EMPTY)],
+    [Cell(0, 2, CellType.WATER), Cell(1, 2, CellType.TEMPLE), Cell(2, 2, CellType.MONSTER)]
+]
+
+test_cell_to_put = [
+    [Cell(0, 0, CellType.EMPTY), Cell(1, 0, CellType.HOUSE)],
+    [Cell(0, 1, CellType.HOUSE), Cell(1, 1, CellType.EMPTY)]
+]
+
+
 def main():
-    cells = [
-        [Cell(0, 0,CellType.FOREST), Cell(1, 0, CellType.MOUNTAIN), Cell(2, 0, CellType.MOUNTAIN)],
-        [Cell(0, 1,CellType.MONSTER), Cell(1, 1, CellType.HOUSE), Cell(2, 1, CellType.MOUNTAIN)],
-        [Cell(0, 2,CellType.WATER), Cell(1, 2, CellType.TEMPLE), Cell(2, 2, CellType.MONSTER)]
-    ]
-    map = Map(3, 3, cells)
+    map = Map(3, 3, test_cell)
+    map.print()
+    map.put_cells_on_map(1, 0, Map.rotate_cells(test_cell_to_put))
     map.print()
 
 
